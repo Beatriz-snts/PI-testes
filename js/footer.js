aa614572758bd4e9505dfbf9571f011aeed5a36c
Beatriz-snts <bvalquiria9882gmail.com>
2025-07-15 11:52:52 -0300

    feat: atividadezinha marota footer

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+const rodape = document.getElementById("footer");
+rodape.innerHTML = `   <nav>
+                            <li><a href="servicos.html">Serviços</a></li>
+                            <li><a href="sobre.html">Sobre</a></li>
+                            <li><a href="contato.html">Contato</a></li>
+                        </nav>´
--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@ nav.innerHTML = `<header id="navbar">
 <img src="img/logo.svg" alt="Logo da pagina" class="logo">
 
 <nav class="navbar-desktop">
-    <a href="index.html">Home</a>
+    <a href="index.html" id="ancora-home">Home</a>
     <a href="sobre.html">Sobre</a>
     <a href="contato.html">Contato</a>
     <a href="serviços.html">Serviços</a>
