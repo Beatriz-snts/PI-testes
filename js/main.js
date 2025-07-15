52811e7221fffc2634bd31bdddef64e117166601
Beatriz-snts <bvalquiria9882gmail.com>
2025-07-15 10:50:38 -0300

    feat: arrumando a pataquada que eu fiz pt2

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const nav =document.getElementById("navbar");
+nav.innerHTML = `<header id="navbar">
+<img src="img/logo.svg" alt="Logo da pagina" class="logo">
+
+<nav class="navbar-desktop">
+    <a href="index.html">Home</a>
+    <a href="sobre.html">Sobre</a>
+    <a href="contato.html">Contato</a>
+    <a href="serviços.html">Serviços</a>
+</nav>
+<ul>
+    <li>
+        <a href="#">
+            <img src="img/facebook.svg" class="redes" alt="Facebook">
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <img src="img/instagram.svg" class="redes" alt="Instagram">
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <img src="img/youtube.svg" class="redes" alt="Youtube">
+        </a>
+    </li>
+</ul>
+</header>
+`
